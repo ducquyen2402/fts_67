d5e3496d224a4d9e7795acdf5337c339cd73baae
leducphuc <leducphuc1994@gmail.com>
2016-09-15 10:27:01 +0700

    admin create subject

--- db/migrate/20160912060436_create_subjects.rb
+++ db/migrate/20160912060436_create_subjects.rb
@@ -2,7 +2,9 @@ class CreateSubjects < ActiveRecord::Migration[5.0]
   def change
     create_table :subjects do |t|
       t.string :name
+      t.float :duration
       t.text :description
+      t.integer :duration, default: 20
 
       t.timestamps
     end
